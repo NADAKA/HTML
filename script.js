b3ae21b22b27c6c7f5405ad2603ef681c236f039
NADAKA <nadaka5471@gmail.com>
2017-02-23 18:16:57 -0600

    tarea2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-var BacoB = angular.module ("BacoB";[]);
+var  armando =  angular.module ("armandoE",[]);
+  armando.controller("Suma",function($scope){
+    $scope.cantidad1 = 2;
+    $scope.cantidad2 = 1;
+    $scope.Suma = function(cantidad){
+      $scope.cantidad1 + $scope.cantidad2
+    };
+  })
+  armando.controller("Miltiplicacion",function($scope){
+    $scope.cantidad1 = 2;
+    $scope.cantidad2 = 1;
+    $scope.Miltiplicacion = function(cantidad){
+      $scope.cantidad1 * $scope.cantidad2
+  })
